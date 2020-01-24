47aa872969efbccddb211520ea738858997d5a7d
Andre Florindo <andreflorindo@hotmail.com>
2020-01-24 18:11:21 +0100

    comment TOTG time parameterization

--- vsl_descartes_moveit/include/vsl_descartes_moveit_planner.h
+++ vsl_descartes_moveit/include/vsl_descartes_moveit_planner.h
@@ -31,7 +31,7 @@
 //  Time Parameterization
 //#include <moveit/trajectory_processing/iterative_time_parameterization.h>
 //#include <moveit/trajectory_processing/iterative_spline_parameterization.h>
-#include <moveit/trajectory_processing/time_optimal_trajectory_generation.h>
+// #include <moveit/trajectory_processing/time_optimal_trajectory_generation.h>
 
 namespace vsl_motion_planning
 {
@@ -101,7 +101,7 @@ protected:
     //Time Parameterization
     //trajectory_processing::IterativeParabolicTimeParameterization time_parameterization_;
     //trajectory_processing::IterativeSplineParameterization time_parameterization_;
-    trajectory_processing::TimeOptimalTrajectoryGeneration time_parameterization_;
+    // trajectory_processing::TimeOptimalTrajectoryGeneration time_parameterization_;
 };
 
 } // namespace vsl_motion_planning
